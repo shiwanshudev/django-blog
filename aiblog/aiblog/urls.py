dc5f9cf2ffb9fd43053dd8779ece8e0005ee367d
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2025-03-27 17:45:36 +0530

    Add posts app setup

--- aiblog/aiblog/urls.py
+++ aiblog/aiblog/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('posts/', include('posts.urls'))
 ]
